561c08ccae0eb7fa0a6b02c1337f79e3018db4f3
msa <mdsohailahmed117@gmail.com>
2024-08-19 02:05:43 +0400

    first

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,22 @@
 import express from "express";
 
+const app = express();
 
-const app=express()
+app.get("/", (req, res) => {
+    res.send("server is running");
+});
 
+app.get("/api/product", (req, res) => {
+    const products = [
+        { id: 1, title: "title1" },
+        { id: 2, title: "title2" },
+        { id: 3, title: "title3" }
+    ];
+    res.send(products);
+});
 
-app.get("/",(req,res)=>{
-    res.send("server is running")
-})
+const port = 8080;
 
-app.get("/api/product",(req,res)=>{
-    const product=[{id:1,title:"title1"},{id:2,title:"title2"},{id:3,title:"title3"}]
-    res.send(product)
-})
-
-const port=8080;
-
-
-app.listen(port,()=>{
-    console.log(`server is running on${port} `)
-})
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
